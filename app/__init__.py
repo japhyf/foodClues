8f8fb7ac5c91ccb86dfe24febc731c80f48622f3
japhyf <root@LAPTOP-IK1J9M5Q.localdomain>
2019-01-20 02:00:39 -0800

    sdafsda

--- app/__init__.py
+++ app/__init__.py
@@ -51,6 +51,7 @@ def create_app(test_config=None):
             'SELECT * FROM Food'
         ).fetchall()
         start = (strLat, strLon)
+        resp.message("youre lat lon is " + str(strLat)+", "+str(strLon))
         minDist = 100000000000000000000000000000
         closestRow = data[0]
         j = 0
