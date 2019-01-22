5a99ead1df950ee6dbdf36aa37d44bcf4836fe2f
japhyf <japhyf@gmail.com>
2019-01-22 12:58:17 -0800

    fixed hacky html, restored full 25,000 entries

--- app/main.py
+++ app/main.py
@@ -16,5 +16,10 @@ def home():
     data = db.execute(
         'SELECT * FROM Food'
     ).fetchall()
-    return render_template('main/home.html',data=data)
-
+    cursor = db.execute('select * from Food')
+    header = [x[0] for x in cursor.description]
+    obj = {}
+    for row in data:
+        obj[row["Address"]] = dict(zip(header,row))
+    y = json.dumps(obj)
+    return render_template('main/home.html', data=y)
